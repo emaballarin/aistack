f102473380a03f2a6b0d7bc2c87614ce2f97d9ac
Emanuele Ballarin <emanuele@ballarin.cc>
2019-06-18 04:44:40 +0200

    Update.
    Forward sync.

--- asrinit.sh
+++ asrinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #############################################
-###  AIStack, v. 3.11.6-001 (17/06/2019)  ###
+###  AIStack, v. 3.11.6-002 (18/06/2019)  ###
 #############################################
 #
 # A hacky-but-effective environment initialization toolkit for Anaconda, aimed
@@ -879,7 +879,7 @@ echo ' '
 pip install --upgrade --no-deps git+https://github.com/fbcotter/py3nvml#egg=py3nvml
 
 echo ' '
-pip install --upgrade --no-deps https://h2o-release.s3.amazonaws.com/h2o/master/4704/Python/h2o-3.25.0.4704-py2.py3-none-any.whl
+pip install --upgrade --no-deps https://h2o-release.s3.amazonaws.com/h2o/master/4705/Python/h2o-3.25.0.4705-py2.py3-none-any.whl
 
 echo ' '
 git clone --recursive https://github.com/Microsoft/TextWorld.git
